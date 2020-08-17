f273ab9f51acd0b265030c85ab8fc7c28f974943
thiago-fiori <thiagofiori12@gmail.com>
2020-08-17 15:53:00 -0300

    Aplicação Client

---
+++ Documents/NetBeansProjects/Client/src/client/Client.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thiag
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            /*
+            1. Estabelecer Conexão com o servidor
+            2. Trocar mensagens com o servidor
+            */
+            //cria a conexão entre o cliente e o servidor
+            System.out.println("Estabelecendo Conexão");
+            Socket socket = new Socket("localhost", 5555);
+            System.out.println("Conexão Estabelecida");
+            
+            //criação dos streams de entrada e saída
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Enviando mensagem...");
+            String msg = "HELLO";
+            output.writeUTF(msg);
+            output.flush(); //libera buffer para envio
+            
+            System.out.println("Mensagem " + '"' + msg + '"' + " enviada");
+            
+            msg = input.readUTF();
+            System.out.println("Resposta: " + msg);
+            
+            input.close();
+            output.close();
+            socket.close();
+            
+        } catch (IOException ex) {
+            System.out.println("Erro no cliente: " + ex);
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+}
